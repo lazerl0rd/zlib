d1310aa2a5c5f3058419eb215bb9982f6e369d95
Diab Neiroukh <officiallazerl0rd@gmail.com>
2020-04-12 18:36:08 +0100

    zutil: Use Intel's RTE memcpy when !SMALL_MEDIUM

--- zutil.h
+++ zutil.h
@@ -224,7 +224,8 @@ extern z_const char * const z_errmsg[10]; /* indexed by 2-zlib_error */
 #    define zmemcmp _fmemcmp
 #    define zmemzero(dest, len) _fmemset(dest, 0, len)
 #  else
-#    define zmemcpy memcpy
+#    include "rte_memcpy.h"
+#    define zmemcpy rte_memcpy
 #    define zmemcmp memcmp
 #    define zmemzero(dest, len) memset(dest, 0, len)
 #  endif
