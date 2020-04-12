0776ed9e23a0b79a26807844b91cb7c2027816c6
Diab Neiroukh <officiallazerl0rd@gmail.com>
2020-04-12 13:32:02 +0100

    crc32: Use the PCLMULQDQ instruction if supported
    According to Shuxin Yang, this provides a speedup of five percent
    in the compression level range of three to six.
    
    Co-authored-by: Shuxin Yang <shuxinyang2006@gmail.com>

--- crc32.c
+++ crc32.c
@@ -234,13 +234,73 @@ unsigned long ZEXPORT crc32_z(crc, buf, len)
 }
 
 /* ========================================================================= */
-unsigned long ZEXPORT crc32(crc, buf, len)
+local unsigned long crc32_generic(crc, buf, len)
     unsigned long crc;
     const unsigned char FAR *buf;
     uInt len;
 {
     return crc32_z(crc, buf, len);
 }
+
+#ifdef HAS_PCLMUL
+
+#define PCLMUL_MIN_LEN 64
+#define PCLMUL_ALIGN 16
+#define PCLMUL_ALIGN_MASK 15
+
+/* Function taken from the Linux kernel 3.14. It computes the CRC over the
+ * given buffer with initial CRC value <crc32>. The buffer is <len> byte in
+ * length, and must be 16-byte aligned.
+ */
+extern uint crc32_pclmul_le_16(unsigned char const *buffer,
+                               size_t len, uInt crc32);
+
+unsigned long crc32(crc, buf, len) unsigned long crc;
+const unsigned char FAR *buf;
+uInt len;
+{
+    if (len < PCLMUL_MIN_LEN + PCLMUL_ALIGN - 1)
+        return crc32_generic(crc, buf, len);
+
+    /* Handle the leading patial chunk */
+    uInt misalign = PCLMUL_ALIGN_MASK & ((unsigned long)buf);
+    uInt sz = (PCLMUL_ALIGN - misalign) % PCLMUL_ALIGN;
+    if (sz)
+    {
+        crc = crc32_generic(crc, buf, sz);
+        buf += sz;
+        len -= sz;
+    }
+
+    /* Go over 16-byte chunks */
+    crc = crc32_pclmul_le_16(buf, (len & ~PCLMUL_ALIGN_MASK),
+                             crc ^ 0xffffffffUL);
+    crc = crc ^ 0xffffffffUL;
+
+    /* Handle the trailing partial chunk */
+    sz = len & PCLMUL_ALIGN_MASK;
+    if (sz)
+    {
+        crc = crc32_generic(crc, buf + len - sz, sz);
+    }
+
+    return crc;
+}
+#undef PCLMUL_MIN_LEN
+#undef PCLMUL_ALIGN
+#undef PCLMUL_ALIGN_MASK
+
+#else /* !HAS_PCLMUL */
+
+unsigned long crc32(crc, buf, len)
+    unsigned long crc;
+    const unsigned char FAR *buf;
+    uInt len;
+{
+    return crc32_generic(crc, buf, len);
+}
+
+#endif /* HAS_PCLMUL */
 
 #ifdef BYFOUR
 
